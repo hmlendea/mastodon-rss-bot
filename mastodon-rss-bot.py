656e8f84d4bfc403d70cc5cc0d668133dcc8ffbd
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-12-07 22:04:37 +0200

    Fixed entry ID calculation in some cases

--- mastodon-rss-bot.py
+++ mastodon-rss-bot.py
@@ -66,7 +66,10 @@ for feed_entry in reversed(feed.entries):
         if len(feed_entry.title) > 0:
             feed_entry_id = feed_entry.title
         else:
-            feed_entry_id = feed_entry.published_parsed
+            if 'published_parsed' in feed_entry:
+                feed_entry_id = str(feed_entry.published_parsed)
+            else:
+                feed_entry_id = str(feed_entry.updated_parsed)
     feed_entry_id = hashlib.md5(feed_entry_id.encode()).hexdigest()
 
     print('Entry found: ' + feed_entry_id)
